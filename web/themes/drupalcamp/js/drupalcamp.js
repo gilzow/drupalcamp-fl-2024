74fc36eed3f8dced5447745cdfa45aebd1830182
Marine Gandy <marine.gandy@platform.sh>
2024-02-22 17:07:36 +0100

    generate subtheme with drush

---
+++ web/themes/drupalcamp/js/drupalcamp.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * drupalcamp behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.drupalcamp = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
